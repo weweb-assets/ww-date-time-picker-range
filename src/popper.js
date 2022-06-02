0c1f80a1d4ca8fa24441bc4121b5c2d001455e9e
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-06-02 09:08:36 +0200

    popperjs

--- src/popper.js
+++ src/popper.js
@@ -43,14 +43,14 @@ function getWindowScroll(node) {
 }
 
 /*:: declare function isElement(node: mixed): boolean %checks(node instanceof
-   Element); */
+    Element); */
 
 function isElement(node) {
   var OwnElement = getWindow(node).Element;
   return node instanceof OwnElement || node instanceof Element;
 }
 /*:: declare function isHTMLElement(node: mixed): boolean %checks(node instanceof
-   HTMLElement); */
+    HTMLElement); */
 
 function isHTMLElement(node) {
   var OwnElement = getWindow(node).HTMLElement;
